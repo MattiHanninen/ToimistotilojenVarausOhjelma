1e5e40f183cdb2d351d75d78ae1a8ae9b85d2017
HoangTran <hoang.a.tran@edu.karelia.fi>
2021-04-21 19:00:43 +0300

    add subpakage named vuokratoimistotDatabase. Using to create databse for application

--- VuokraToimistotApplication/src/vuokratoimistotDatabase/vuokratoimistoDatabase.java
+++ VuokraToimistotApplication/src/vuokratoimistotDatabase/vuokratoimistoDatabase.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package vuokratoimistotapplication.Tietokanta;
+package vuokratoimistotDatabase;
 
 /**
  *
  * @author hoang
  */
-public class VuokraToimistoTietokanta {
+public class vuokratoimistoDatabase {
     
 }
