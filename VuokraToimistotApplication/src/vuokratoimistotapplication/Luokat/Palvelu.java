5e7638a246595fcc34d7fcce1cedbe7c9072398f
Janne Peiponen <janne.peiponen@edu.karelia.fi>
2021-04-23 19:12:56 +0300

    Palvelu.java lisätty setterit ja getterit

--- VuokraToimistotApplication/src/vuokratoimistotapplication/Luokat/Palvelu.java
+++ VuokraToimistotApplication/src/vuokratoimistotapplication/Luokat/Palvelu.java
@@ -7,8 +7,57 @@ package vuokratoimistotapplication.Luokat;
 
 /**
  *
- * @author hoang
+ * @author hoang & janne
  */
 public class Palvelu {
+    
+    private int palvelunID;
+    private String palvelunNimi;
+    private int palvelunHinta;
+    private String palvelunKuvaus;
+    
+    public Palvelu(){
+        
+    }
+
+    //Setterit ja getterit
+    public void setPalvelunID(int palvelunID) {
+        this.palvelunID = palvelunID;
+    }
+
+    public void setPalvelunNimi(String palvelunNimi) {
+        this.palvelunNimi = palvelunNimi;
+    }
+
+    public void setPalvelunHinta(int palvelunHinta) {
+        this.palvelunHinta = palvelunHinta;
+    }
+
+    public void setPalvelunKuvaus(String palvelunKuvaus) {
+        this.palvelunKuvaus = palvelunKuvaus;
+    }
+   
+
+
+    public int getPalvelunID() {
+        return palvelunID;
+    }
+
+    public String getPalvelunNimi() {
+        return palvelunNimi;
+    }
+
+    public int getPalvelunHinta() {
+        return palvelunHinta;
+    }
+
+    public String getPalvelunKuvaus() {
+        return palvelunKuvaus;
+    }
+    
+     @Override
+    public String toString() {
+        return (palvelunID + " " + palvelunNimi + " " + palvelunHinta + " " + palvelunKuvaus);
+    }
     
 }
