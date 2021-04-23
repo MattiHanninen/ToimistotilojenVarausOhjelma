2112717c5e4dd614e6fdbc3e27d8d8e1aae015c6
Janne Peiponen <janne.peiponen@edu.karelia.fi>
2021-04-23 19:17:19 +0300

    Toimipiste.java lisätty setterit ja getterit

--- VuokraToimistotApplication/src/vuokratoimistotapplication/Luokat/Toimipiste.java
+++ VuokraToimistotApplication/src/vuokratoimistotapplication/Luokat/Toimipiste.java
@@ -7,8 +7,55 @@ package vuokratoimistotapplication.Luokat;
 
 /**
  *
- * @author hoang
+ * @author hoang & janne
  */
 public class Toimipiste {
+ 
+    private int toimipisteID;
+    private String toimipisteNimi;
+    private int vuorokausiHinta;
+    private int toimipisteKoko;
+    
+    public Toimipiste(){
+        
+    }
+
+    //Setterit ja getterit
+    public void setToimipisteID(int toimipisteID) {
+        this.toimipisteID = toimipisteID;
+    }
+
+    public void setToimipisteNimi(String toimipisteNimi) {
+        this.toimipisteNimi = toimipisteNimi;
+    }
+
+    public void setVuorokausiHinta(int vuorokausiHinta) {
+        this.vuorokausiHinta = vuorokausiHinta;
+    }
+
+    public void setToimipisteKoko(int toimipisteKoko) {
+        this.toimipisteKoko = toimipisteKoko;
+    }
+
+    public int getToimipisteID() {
+        return toimipisteID;
+    }
+
+    public String getToimipisteNimi() {
+        return toimipisteNimi;
+    }
+
+    public int getVuorokausiHinta() {
+        return vuorokausiHinta;
+    }
+
+    public int getToimipisteKoko() {
+        return toimipisteKoko;
+    }
+
+    @Override
+    public String toString() {
+        return (toimipisteID + " " + toimipisteNimi + " " + vuorokausiHinta + " " + toimipisteKoko);
+    }
     
 }
