7177c1f957d0fb0d78cdc301a2200ba1e8d9c43c
Janne Peiponen <janne.peiponen@edu.karelia.fi>
2021-04-23 19:08:44 +0300

    lasku.class lisätty setterit ja getterit

--- VuokraToimistotApplication/src/vuokratoimistotapplication/Luokat/Lasku.java
+++ VuokraToimistotApplication/src/vuokratoimistotapplication/Luokat/Lasku.java
@@ -5,10 +5,78 @@
  */
 package vuokratoimistotapplication.Luokat;
 
+import java.util.Date;
+
 /**
  *
- * @author hoang
+ * @author hoang & janne
  */
 public class Lasku {
+    
+    private int laskuID;
+    private Date erapaiva;
+    private Date maksupaiva;
+    private int summa;
+    private int viitenumero;
+    private String laskutusTyyppi;
+    
+    public Lasku(){
+        
+    }
+    
+    //Getterit ja setterit
+    public int getLaskuID() {
+        return laskuID;
+    }
+
+    public Date getErapaiva() {
+        return erapaiva;
+    }
+
+    public Date getMaksupaiva() {
+        return maksupaiva;
+    }
+
+    public int getSumma() {
+        return summa;
+    }
+
+    public int getViitenumero() {
+        return viitenumero;
+    }
+
+    public String getLaskutusTyyppi() {
+        return laskutusTyyppi;
+    }
+    
+
+    public void setLaskuID(int laskuID) {
+        this.laskuID = laskuID;
+    }
+
+    public void setErapaiva(Date erapaiva) {
+        this.erapaiva = erapaiva;
+    }
+
+    public void setMaksupaiva(Date maksupaiva) {
+        this.maksupaiva = maksupaiva;
+    }
+
+    public void setSumma(int summa) {
+        this.summa = summa;
+    }
+
+    public void setViitenumero(int viitenumero) {
+        this.viitenumero = viitenumero;
+    }
+
+    public void setLaskutusTyyppi(String laskutusTyyppi) {
+        this.laskutusTyyppi = laskutusTyyppi;
+    }
+    
+    @Override
+    public String toString() {
+        return (laskuID + " " + erapaiva + " " + maksupaiva + " " + summa + " " + viitenumero + " " + laskutusTyyppi);
+    }
     
 }
