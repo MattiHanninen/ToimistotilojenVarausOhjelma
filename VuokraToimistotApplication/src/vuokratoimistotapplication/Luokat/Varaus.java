c123ffa0f0f0f02cec2511e6ba9f7d41b51281f6
Janne Peiponen <janne.peiponen@edu.karelia.fi>
2021-04-23 19:23:00 +0300

    Varaus.java lisätty setterit ja getterit

--- VuokraToimistotApplication/src/vuokratoimistotapplication/Luokat/Varaus.java
+++ VuokraToimistotApplication/src/vuokratoimistotapplication/Luokat/Varaus.java
@@ -5,10 +5,70 @@
  */
 package vuokratoimistotapplication.Luokat;
 
+import java.util.Date;
+
 /**
  *
- * @author hoang
+ * @author hoang & janne
  */
 public class Varaus {
+    
+    private int varausID;
+    private Date aloitusPaiva;
+    private Date lopetusPaiva;
+    private int asiakasID;
+    private int toimipisteID;
+    
+    public Varaus(){
+        
+    }
+    
+
+    //Setterit ja getterit
+    public void setVarausID(int varausID) {
+        this.varausID = varausID;
+    }
+
+    public void setAloitusPaiva(Date aloitusPaiva) {
+        this.aloitusPaiva = aloitusPaiva;
+    }
+
+    public void setLopetusPaiva(Date lopetusPaiva) {
+        this.lopetusPaiva = lopetusPaiva;
+    }
+
+    public void setAsiakasID(int asiakasID) {
+        this.asiakasID = asiakasID;
+    }
+
+    public void setToimipisteID(int toimipisteID) {
+        this.toimipisteID = toimipisteID;
+    }
+   
+
+    public int getVarausID() {
+        return varausID;
+    }
+
+    public Date getAloitusPaiva() {
+        return aloitusPaiva;
+    }
+
+    public Date getLopetusPaiva() {
+        return lopetusPaiva;
+    }
+
+    public int getAsiakasID() {
+        return asiakasID;
+    }
+
+    public int getToimipisteID() {
+        return toimipisteID;
+    }
+    
+    @Override
+    public String toString() {
+        return (varausID + " " + aloitusPaiva + " " + lopetusPaiva + " " + asiakasID + " " + toimipisteID);
+    }
     
 }
