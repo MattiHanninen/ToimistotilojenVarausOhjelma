ff636e79d291b5fcba3b55ee053281feddb7c5d8
Hoang Tran <1804950@edu.karelia.fi>
2021-04-26 08:50:03 +0000

    Updated vuokratoimistoDatabase.java

--- VuokraToimistotApplication/src/vuokratoimistotDatabase/vuokratoimistoDatabase.java
+++ VuokraToimistotApplication/src/vuokratoimistotDatabase/vuokratoimistoDatabase.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package vuokratoimistotDatabase;
 
 import java.sql.Connection;
@@ -54,7 +50,7 @@ public class vuokratoimistoDatabase {
 
         Statement stmt = c.createStatement();
         stmt.executeQuery(sql);
-        System.out.println("\t>> Taulu " + tauluNimi + " luotu");
+        System.out.println("\n" + "\t>> Taulu " + tauluNimi + " luotu");
     }
     
     //Metodi joka asettaa tietokannan valituksi
@@ -99,7 +95,6 @@ public class vuokratoimistoDatabase {
         
         ps.execute();       
         System.out.println("\t>> Lisätty tyontekija: " + etunimi +" "+ sukunimi);
-        
     }
     
     //Metodi joka lisätä tietokannan tyontekijat
@@ -122,6 +117,132 @@ public class vuokratoimistoDatabase {
         
     }
     
+    //Metodi joka lisätä tietokannan varaukset 
+    private static void addVaraus(Connection c, int varausID, String aloitusPaiva,String lopetusPaiva,
+                                    int asiakasID, int toimipisteID )throws SQLException {   
+        
+        PreparedStatement ps = c.prepareStatement
+        (
+        "INSERT INTO varaus (varausID, aloitusPaiva, lopetusPaiva, asiakasID, toimipisteID)"
+         + "VALUES(?, STR_TO_DATE(?, '%d.%m.%Y'), STR_TO_DATE(?, '%d.%m.%Y'), ?, ?)"
+        );
+        
+        ps.setInt(1, varausID);
+        ps.setString(2, aloitusPaiva);
+        ps.setString(3, lopetusPaiva);
+        ps.setInt(4, asiakasID);
+        ps.setInt(5, toimipisteID);
+            
+        ps.execute();       
+        System.out.println("\t>> Lisätty varaus: " + varausID );
+        
+    }
+    
+        //Metodi joka lisätä tietokannan laskut
+    private static void addLasku(Connection c, int laskuID, String erapaiva, String maksupaiva,
+                                    int summa, int viitenumero, String laskutusTyyppi)throws SQLException {   
+        
+        PreparedStatement ps = c.prepareStatement
+        (
+        "INSERT INTO lasku (laskuID, erapaiva, maksupaiva, summa, viitenumero,laskutusTyyppi)"
+         + "VALUES(?, STR_TO_DATE(?, '%d.%m.%Y'), STR_TO_DATE(?, '%d.%m.%Y'), ?, ?, ?)"
+        );
+        
+        ps.setInt(1, laskuID);
+        ps.setString(2, erapaiva);
+        ps.setString(3, maksupaiva);
+        ps.setInt(4, summa);
+        ps.setInt(5, viitenumero);
+        ps.setString(6, laskutusTyyppi);
+            
+        ps.execute();       
+        System.out.println("\t>> Lisätty lasku: " + laskuID + " " + laskutusTyyppi);
+        
+    }
+    //Metodi joka lisätä tietokannan varaukset 
+    private static void addPalvelu(Connection c, int palvelunID, String palvelunNimi,int palvelunHinta)throws SQLException {   
+        
+        PreparedStatement ps = c.prepareStatement
+        (
+        "INSERT INTO palvelu (palvelunID, palvelunNimi, palvelunHinta)"
+         + "VALUES(?, ?, ?)"
+        );
+        
+        ps.setInt(1, palvelunID);
+        ps.setString(2, palvelunNimi);
+        ps.setInt(3, palvelunHinta);
+            
+        ps.execute();       
+        System.out.println("\t>> Lisätty palvelu: " + palvelunID + " " + palvelunNimi);
+        
+    }
+    
+    
+    //Metodi joka lisätä tietokannan varaukset 
+    private static void addToimipisteidenPalvelut(Connection c, int toimipisteID, int palvelunID)throws SQLException {   
+        
+        PreparedStatement ps = c.prepareStatement
+        (
+        "INSERT INTO toimipisteidenPalvelut(toimipisteID, palvelunID)"
+         + "VALUES(?, ?)"
+        );
+        
+        ps.setInt(1, toimipisteID);
+        ps.setInt(2, palvelunID);
+                  
+        ps.execute();       
+        System.out.println("\t>> Lisätty toimipisteidenPalvelut: " + palvelunID );       
+    }
+    
+    //Metodi joka lisätä tietokannan laskun maksajat 
+    private static void addlaskunMaksaja(Connection c, int asiakasID, int laskuID)throws SQLException {   
+        
+        PreparedStatement ps = c.prepareStatement
+        (
+        "INSERT INTO laskunMaksaja(asiakasID, laskuID)"
+         + "VALUES(?, ?)"
+        );
+        
+        ps.setInt(1, asiakasID);
+        ps.setInt(2, laskuID);
+                  
+        ps.execute();       
+        System.out.println("\t>> Lisätty laskunMaksaja taulu: " + asiakasID + " " + laskuID );       
+    }
+    
+    // Metodi joka lisätä tietokannan laskun maksajat 
+    private static void addLaskunKasittelija(Connection c, int tyontekijaID, int laskuID)throws SQLException {   
+        
+        PreparedStatement ps = c.prepareStatement
+        (
+        "INSERT INTO laskunKasittelija(tyontekijaID, laskuID)"
+         + "VALUES(?, ?)"
+        );
+        
+        ps.setInt(1, tyontekijaID);
+        ps.setInt(2, laskuID);
+                  
+        ps.execute();       
+        System.out.println("\t>> Lisätty laskunMaksaja taulu: " + tyontekijaID + " " + laskuID );       
+    }
+    
+    
+    
+    // Metodi joka lisätä tietokannan laskun maksajat 
+    private static void addVarauksenKasittelija(Connection c, int tyontekijaID, int varausID)throws SQLException {   
+        
+        PreparedStatement ps = c.prepareStatement
+        (
+        "INSERT INTO varauksenKasittelija(tyontekijaID, varausID)"
+         + "VALUES(?, ?)"
+        );
+        
+        ps.setInt(1, tyontekijaID);
+        ps.setInt(2, varausID);
+                  
+        ps.execute();       
+        System.out.println("\t>> Lisätty laskunMaksaja taulu: " + tyontekijaID + " " + varausID );       
+    }
     
     /**
      * @param args
@@ -179,9 +300,108 @@ public class vuokratoimistoDatabase {
         addToimipiste(conn, 60160, "Linnunlahti", 1200, 65);
         addToimipiste(conn, 60100, "Rantakylä", 800, 70);
         
+        //Lasku taulu luonti
+        createTable(conn, 
+                "CREATE TABLE lasku ("
+                        + "laskuID INT NOT NULL PRIMARY KEY," 
+                        + "erapaiva DATE NOT NULL,"
+                        + "maksupaiva DATE NOT NULL,"
+                        + "summa INT NOT NULL,"       
+                        + "viitenumero INT NOT NULL,"
+                        + "laskutusTyyppi VARCHAR(30) NOT NULL)"
+                        ,
+                "lasku"
+                );
+        addLasku(conn, 55025, "12.3.2021", "10.3.2021", 599, 123789, "paperilasku");
+        addLasku(conn, 56030, "28.1.2021", "2.1.2020", 1010, 345234, "sähköpostilasku");
         
+        
+        
+        //Palvelu taulu luonti
+        createTable(conn, 
+                "CREATE TABLE palvelu ("
+                        + "palvelunID INT NOT NULL PRIMARY KEY," 
+                        + "palvelunNimi VARCHAR(30) NOT NULL,"
+                        + "palvelunHinta INT NOT NULL)"        
+                        ,
+                "palvelu"
+                );
+        addPalvelu(conn, 1, "Yksilö" , 199);
+        addPalvelu(conn, 2, "Ekologi", 299);
 
+        //Varaus taulu luonti
+        createTable(conn, 
+                "CREATE TABLE varaus ("
+                        + "varausID INT NOT NULL PRIMARY KEY," 
+                        + "aloitusPaiva DATE NOT NULL,"
+                        + "lopetusPaiva DATE NOT NULL,"
+                        + "asiakasID INT NOT NULL," 
+                        + "toimipisteID INT NOT NULL,"
+                        + "FOREIGN KEY (asiakasID) REFERENCES asiakas(asiakasID) ON DELETE CASCADE,"
+                        + "FOREIGN KEY (toimipisteID) REFERENCES toimipiste(toimipisteID) ON DELETE CASCADE)"
+                        ,
+                "varaus"
+                );
+
+        addVaraus(conn, 3000, "10.3.2021", "12.3.2021", 1100, 60160);
+        addVaraus(conn, 3001, "12.4.2021", "15.4.2021", 1101, 60100);
         
+        //ToimipistePalvelu taulu luonti
+        createTable(conn, 
+                "CREATE TABLE toimipisteidenPalvelut ("
+                        + "toimipisteID INT NOT NULL," 
+                        + "palvelunID INT NOT NULL,"
+                        + "PRIMARY KEY (toimipisteID, palvelunID),"                      
+                        + "FOREIGN KEY (toimipisteID) REFERENCES toimipiste(toimipisteID) ON DELETE CASCADE,"
+                        + "FOREIGN KEY (palvelunID) REFERENCES palvelu(palvelunID) ON DELETE CASCADE)"
+                        ,
+                "toimipisteidenPalvelut"
+                );
+        addToimipisteidenPalvelut(conn, 60160, 1);
+        addToimipisteidenPalvelut(conn, 60100, 2);
+        
+        //LaskunMaksaja taulu luonti
+        createTable(conn, 
+                "CREATE TABLE laskunMaksaja ("
+                        + "asiakasID INT NOT NULL," 
+                        + "laskuID INT NOT NULL,"
+                        + "PRIMARY KEY (asiakasID, laskuID),"                      
+                        + "FOREIGN KEY (asiakasID) REFERENCES asiakas(asiakasID) ON DELETE CASCADE,"
+                        + "FOREIGN KEY (laskuID) REFERENCES lasku(laskuID) ON DELETE CASCADE)"
+                        ,
+                "laskunMaksaja"
+                );
+        addlaskunMaksaja(conn, 1100, 55025);
+        addlaskunMaksaja(conn, 1101, 56030);
+        
+        //LaskunKasittelija taulu luonti
+        createTable(conn, 
+                "CREATE TABLE laskunKasittelija ("
+                        + "tyontekijaID INT NOT NULL," 
+                        + "laskuID INT NOT NULL,"
+                        + "PRIMARY KEY (tyontekijaID, laskuID),"                      
+                        + "FOREIGN KEY (tyontekijaID) REFERENCES tyontekija(tyontekijaID) ON DELETE CASCADE,"
+                        + "FOREIGN KEY (laskuID) REFERENCES lasku(laskuID) ON DELETE CASCADE)"
+                        ,
+                "laskunKasittelija"
+                );
+        addLaskunKasittelija(conn, 2200, 55025);
+        addLaskunKasittelija(conn, 2201, 56030);
+        
+        
+        //VarauksenKasittelija taulu luonti
+        createTable(conn, 
+                "CREATE TABLE varauksenKasittelija ("
+                        + "tyontekijaID INT NOT NULL," 
+                        + "varausID INT NOT NULL,"
+                        + "PRIMARY KEY (tyontekijaID, varausID),"                      
+                        + "FOREIGN KEY (tyontekijaID) REFERENCES tyontekija(tyontekijaID) ON DELETE CASCADE,"
+                        + "FOREIGN KEY (varausID) REFERENCES varaus(varausID) ON DELETE CASCADE)"
+                        ,
+                "varauksenKasittelija"
+                );
+        addVarauksenKasittelija(conn, 2200, 3000);
+        addVarauksenKasittelija(conn, 2201, 3001);
         
     }
 }
