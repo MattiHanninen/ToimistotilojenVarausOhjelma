235e6b52b3ce49e34c019e3add07334581bc8eea
Hoang Tran <1804950@edu.karelia.fi>
2021-04-27 07:25:27 +0000

    Poistetaan tietokanta main metodista. Tietokanta on nyt vuokratoimistotDatabase:ssa

--- VuokraToimistotApplication/src/vuokratoimistotapplication/VuokraToimistotApplication.java
+++ VuokraToimistotApplication/src/vuokratoimistotapplication/VuokraToimistotApplication.java
@@ -20,82 +20,7 @@ import javafx.stage.Stage;
  * @author matty
  */
 public class VuokraToimistotApplication extends Application {
-    
-    
-    /*
-    * Metodi joka avaa tietokantayhteyden
-    */    
-    public static Connection openConnection(String connString) throws SQLException {
-        Connection con = DriverManager.getConnection(connString);
-        System.out.println("\t>> Yhteys ok");
-        return con;
-    }
-    
-      /**
-     * Metodi joka sulkee tietokantayhteyden
-     * @param c Yhteys
-     * @throws java.sql.SQLException SQL poikkeus
-     */
-    public static void closeConnection(Connection c) throws SQLException {
-        if (c != null) {
-            c.close();
-        }
-        System.out.println("\t>> Tietokantayhteys suljettu");
-    }
-    
-     /**
-     * Metodi joka poistaa tietokannan, jos se on olemassa, jonka jalkeen luo uuden
-     * @param c Yhteys
-     * @param db Tietokanta
-     * @throws SQLException SQL poikkeus
-     */
-
-    public static void createDatabase(Connection c, String db) throws SQLException {
-
-        Statement stmt = c.createStatement();
-        stmt.executeQuery("DROP DATABASE IF EXISTS " + db);
-        System.out.println("\t>> Tietokanta " + db + " tuhottu");
-
-        stmt.executeQuery("CREATE DATABASE " + db);
-        System.out.println("\t>> Tietokanta " + db + " luotu");
-
-        stmt.executeQuery("USE " + db);
-        System.out.println("\t>> Kaytetaan tietokantaa " + db);
-
-    }
-    
-     /**
-     * Metodi joka luo uuden taulun
-     * @param c Yhteys
-     * @param sql SQL lause
-     * @throws SQLException SQL poikkeus
-     */
-    public static void createTable(Connection c, String sql) throws SQLException {
-
-        Statement stmt = c.createStatement();
-        stmt.executeQuery(sql);
-        System.out.println("\t>> Taulu luotu");
-    }
-    
-     /**
-     * Metodi joka asettaa tietokannan valituksi
-     * @param c Yhteys
-     * @param db Tietokanta
-     * @throws SQLException SQL poikkeus
-     */
-    public static void useDatabase(Connection c, String db) throws SQLException {
-        Statement stmt = c.createStatement();
-        stmt.executeQuery("USE " + db);
-        System.out.println("\t>> Käytetään tietokantaa " + db);
-    }
-    
-    
-    
-    
-    
-    
-    
-    
+         
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("PaavalikkoView.fxml"));
@@ -111,34 +36,6 @@ public class VuokraToimistotApplication extends Application {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws SQLException {
-        
-        // Luodaan Connection String olemassa olevaan tietokantaan
-        Connection conn = openConnection("jdbc:mariadb://maria.westeurope.cloudapp.azure.com:"
-                + "3306?user=opiskelija&password=opiskelija1");
-
-        // Luodaan tietokanta
-        createDatabase(conn, "karelia_vuokratoimistot_R01");
-
-        // Otetaan tietokanta kayttoon
-        VuokraToimistotApplication.useDatabase(conn, "karelia_vuokratoimistot_R01");
-        
-        // Seuraavaksi taulujen luonti metodien avulla
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        // Suljetaan yhteys
-        closeConnection(conn);
         
         launch(args);
     }
