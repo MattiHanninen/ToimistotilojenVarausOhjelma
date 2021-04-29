a0a5eb5529f4baca7a4bc4e827ccd59174f3f500
Janne Peiponen <janne.peiponen@edu.karelia.fi>
2021-04-29 17:25:24 +0300

    scenebuilderilla viimeistelty

--- VuokraToimistotApplication/src/vuokratoimistotapplication/LaskutusHallintaViewController.java
+++ VuokraToimistotApplication/src/vuokratoimistotapplication/LaskutusHallintaViewController.java
@@ -7,14 +7,46 @@ package vuokratoimistotapplication;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
  *
- * @author matty
+ * @author Janne & matty
  */
 public class LaskutusHallintaViewController implements Initializable {
+
+    @FXML
+    private TextField txfLaskuID;
+    @FXML
+    private TextField txfAsiakasID;
+    @FXML
+    private TextField txfErapaiva;
+    @FXML
+    private TextField txfMaksupaiva;
+    @FXML
+    private TextField txfSumma;
+    @FXML
+    private TextField txfMaksettu;
+    @FXML
+    private TextField txfLaskutusTyyppi;
+    @FXML
+    private TableColumn<?, ?> colLaskuID;
+    @FXML
+    private TableColumn<?, ?> colAsiakasID;
+    @FXML
+    private TableColumn<?, ?> colErapaiva;
+    @FXML
+    private TableColumn<?, ?> colMaksupaiva;
+    @FXML
+    private TableColumn<?, ?> colSumma;
+    @FXML
+    private TableColumn<?, ?> colMaksettu;
+    @FXML
+    private TableColumn<?, ?> colLaskutustyyppi;
 
     /**
      * Initializes the controller class.
