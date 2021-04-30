3ef8bac5b6679c05aa68e9bca8f8ba79bb3e547c
Janne Peiponen <janne.peiponen@edu.karelia.fi>
2021-04-30 15:48:45 +0300

    Tehty laskujen poisto, muokkaus ja tableviewta klikkaamalla täyttyvät textfieldit

--- VuokraToimistotApplication/src/vuokratoimistotapplication/LaskutusHallintaViewController.java
+++ VuokraToimistotApplication/src/vuokratoimistotapplication/LaskutusHallintaViewController.java
@@ -11,6 +11,7 @@ import vuokratoimistotDatabase.vuokratoimistoDatabase;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.Date;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -134,6 +135,52 @@ public class LaskutusHallintaViewController implements Initializable {
         }
     }
     
+    public static void deleteLasku(Connection c, int laskuID) throws SQLException {
+        PreparedStatement ps = c.prepareStatement( 
+        ("DELETE FROM lasku WHERE laskuID=?")
+                
+    );
+    
+        //parametri jonka mukaan poistetaan
+        ps.setInt(1, laskuID);
+    
+        //toteutetaan delete toiminto
+        ps.execute();
+        System.out.println("\t>> poistettu opiskelija_id " + laskuID);
+   
+    }
+    
+    //muokataan opiskelijaa
+    public static void editLasku(Connection c, int laskuID, int asiakasID, String erapaiva, String maksupaiva,
+            int summa, int maksettu, String laskutusTyyppi) throws SQLException {
+        
+        //PreparedStatement ps = c.prepareStatement(
+        //("UPDATE lasku SET asiakasID=?, erapaiva=?, maksupaiva=?, summa=?, maksettu=?, tilausTapa=? WHERE laskuID=?")
+        //);
+        // ("UPDATE asiakas SET etunimi=?, sukunimi=?, yritys=? WHERE asiakasID=?"));
+        
+         PreparedStatement ps = c.prepareStatement(
+        ("UPDATE lasku SET asiakasID=?, erapaiva=STR_TO_DATE(?, '%d.%m.%Y'), maksupaiva=STR_TO_DATE(?, '%d.%m.%Y'), summa=?, maksettu=?, laskutusTyyppi=? WHERE laskuID=?")
+        );
+        //"INSERT INTO lasku (laskuID, asiakasID, erapaiva, maksupaiva, summa, maksettu,laskutusTyyppi)"
+        // + "VALUES(?, ?, STR_TO_DATE(?, '%d.%m.%Y'), STR_TO_DATE(?, '%d.%m.%Y'), ?, ?, ?)"
+      
+        //Laitetaan oikeat parametrit
+        ps.setInt(1,asiakasID);
+        ps.setString(2,erapaiva);
+        ps.setString(3,maksupaiva);
+        ps.setInt(4,summa);
+        ps.setInt(5,maksettu);
+        ps.setString(6,laskutusTyyppi);
+        ps.setInt(7,laskuID); 
+   
+        //Toteutetaan muutokset
+        ps.execute();
+    
+        System.out.println("\t>> Päivitetty laskuID tiedot: " + laskuID);
+        
+    }
+    
     //valitsee asiakkaiden tiedot tableview:n täyttämistä varten
      public static ResultSet selectLasku(Connection c) throws SQLException {
         Statement stmt = c.createStatement();
@@ -169,15 +216,87 @@ public class LaskutusHallintaViewController implements Initializable {
     }
 
     @FXML
-    private void btnMuokkaaClicked(ActionEvent event) {
+    private void btnMuokkaaClicked(ActionEvent event) throws SQLException {
+         // Luodaan Connection String olemassa olevaan tietokantaan
+        Connection conn = vuokratoimistoDatabase.openConnection("jdbc:mariadb://maria.westeurope.cloudapp.azure.com:"
+                    + "3306?user=opiskelija&password=opiskelija1");
+               
+        // Otetaan tietokanta kayttoon
+        vuokratoimistoDatabase.useDatabase(conn, "karelia_vuokratoimistot_R01");
+        
+        //muokkaa asiakkaan tiedot
+        
+        editLasku(conn, Integer.parseInt(txfLaskuID.getText()), Integer.parseInt(txfAsiakasID.getText()), txfErapaiva.getText(),
+                txfMaksupaiva.getText(), Integer.parseInt(txfSumma.getText()), Integer.parseInt(txfMaksettu.getText()), txfLaskutusTyyppi.getText());
+        
+        //Tyhjennetään aiempi Laskujen tableview
+        clearTableviewLasku();
+        
+        //Paivitetaan Laskujen tableview taulu
+        updateTableviewLasku();
+        
+        //Suljetaan yhteys
+        vuokratoimistoDatabase.closeConnection(conn);
     }
 
     @FXML
-    private void btnPoistaClicked(ActionEvent event) {
+    private void btnPoistaClicked(ActionEvent event) throws SQLException {
+         // Luodaan Connection String olemassa olevaan tietokantaan
+        Connection conn = vuokratoimistoDatabase.openConnection("jdbc:mariadb://maria.westeurope.cloudapp.azure.com:"
+                    + "3306?user=opiskelija&password=opiskelija1");
+               
+        // Otetaan tietokanta kayttoon
+        vuokratoimistoDatabase.useDatabase(conn, "karelia_vuokratoimistot_R01");
+        
+        //Poistetaan asiakkaan tiedot
+        deleteLasku(conn, Integer.parseInt(txfLaskuID.getText()));
+        
+        //Tyhjennetään aiempi Laskujen tableview
+        clearTableviewLasku();
+        
+        //Paivitetaan Laskujen tableview taulu
+        updateTableviewLasku();
+        
+        //Suljetaan yhteys
+        vuokratoimistoDatabase.closeConnection(conn);
+      
     }
 
+    /**
+     * TableViewta klikatessa hiirellä kopioituvat tiedot textfieldeihin
+     * @param event Lasku-taulukkoa klikataan
+     * @throws SQLException 
+     */
     @FXML
-    private void tblLaskuClickedFillTextfield(MouseEvent event) {
+    private void tblLaskuClickedFillTextfield(MouseEvent event) throws SQLException {
+        // Luodaan Connection String olemassa olevaan tietokantaan
+        Connection conn = vuokratoimistoDatabase.openConnection("jdbc:mariadb://maria.westeurope.cloudapp.azure.com:"
+                    + "3306?user=opiskelija&password=opiskelija1");
+             
+        // Otetaan tietokanta kayttoon
+        vuokratoimistoDatabase.useDatabase(conn, "karelia_vuokratoimistot_R01");
+   
+        Lasku user = tableLasku.getSelectionModel().getSelectedItem();
+          
+        PreparedStatement pst = conn.prepareStatement(
+        ("SELECT * from lasku where laskuID =?")
+        );
+        
+        pst.setInt(1, user.getLaskuID());
+        ResultSet rs = pst.executeQuery();
+           
+           while(rs.next()){
+               txfLaskuID.setText(rs.getString("laskuID"));
+               txfAsiakasID.setText(rs.getString("asiakasID"));
+               txfErapaiva.setText(rs.getString("erapaiva"));
+               txfMaksupaiva.setText(rs.getString("maksupaiva"));
+               txfSumma.setText(rs.getString("summa"));
+               txfMaksettu.setText(rs.getString("maksettu"));
+               txfLaskutusTyyppi.setText(rs.getString("laskutusTyyppi"));
+           }
+    
+        //Suljetaan yhteys
+        vuokratoimistoDatabase.closeConnection(conn);
     }
     
 }
