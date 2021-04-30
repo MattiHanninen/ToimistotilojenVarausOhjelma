36d1d71caf261250441004540179fd7a7c592689
Janne Peiponen <janne.peiponen@edu.karelia.fi>
2021-04-30 10:12:16 +0300

    Tehty Laskuista tableview ja imporattu Lasku-luokkaan myös sgl.date

--- VuokraToimistotApplication/src/vuokratoimistotapplication/LaskutusHallintaViewController.java
+++ VuokraToimistotApplication/src/vuokratoimistotapplication/LaskutusHallintaViewController.java
@@ -10,10 +10,13 @@ import vuokratoimistotDatabase.vuokratoimistoDatabase;
 
 import java.net.URL;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -21,6 +24,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+
 
 /**
  * FXML Controller class
@@ -44,19 +49,19 @@ public class LaskutusHallintaViewController implements Initializable {
     @FXML
     private TextField txfLaskutusTyyppi;
     @FXML
-    private TableColumn<?, ?> colLaskuID;
+    private TableColumn<Lasku, Integer> colLaskuID;
     @FXML
-    private TableColumn<?, ?> colAsiakasID;
+    private TableColumn<Lasku, Integer> colAsiakasID;
     @FXML
-    private TableColumn<?, ?> colErapaiva;
+    private TableColumn<Lasku, Date> colErapaiva;
     @FXML
-    private TableColumn<?, ?> colMaksupaiva;
+    private TableColumn<Lasku, Date> colMaksupaiva;
     @FXML
-    private TableColumn<?, ?> colSumma;
+    private TableColumn<Lasku, Integer> colSumma;
     @FXML
-    private TableColumn<?, ?> colMaksettu;
+    private TableColumn<Lasku, Integer> colMaksettu;
     @FXML
-    private TableColumn<?, ?> colLaskutustyyppi;
+    private TableColumn<Lasku, String> colLaskutustyyppi;
     @FXML
     private Button btnLisaa;
     @FXML
@@ -64,13 +69,16 @@ public class LaskutusHallintaViewController implements Initializable {
     @FXML
     private Button btnPoista;
     @FXML
-    private TableView<?> tableLasku;
+    private TableView<Lasku> tableLasku;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        
+        updateTableviewLasku();
+        
         //prompt text ei näy ekassa textfielissä, koska se valitaan ekaksi controlliksi, siksi setFocusTraversable(false) metodi.
         txfLaskuID.setFocusTraversable(false);
         txfAsiakasID.setFocusTraversable(false);
@@ -80,6 +88,43 @@ public class LaskutusHallintaViewController implements Initializable {
         txfMaksettu.setFocusTraversable(false);
         txfLaskutusTyyppi.setFocusTraversable(false);
     }    
+    
+     //tableview syöttää tauluun tiedot
+    public void updateTableviewLasku(){
+        
+        colLaskuID.setCellValueFactory(new PropertyValueFactory<>("laskuID"));
+        colAsiakasID.setCellValueFactory(new PropertyValueFactory<>("asiakasID"));
+        colErapaiva.setCellValueFactory(new PropertyValueFactory<>("erapaiva"));
+        colMaksupaiva.setCellValueFactory(new PropertyValueFactory<>("maksupaiva"));
+        colSumma.setCellValueFactory(new PropertyValueFactory<>("summa"));
+        colMaksettu.setCellValueFactory(new PropertyValueFactory<>("maksettu"));
+        colLaskutustyyppi.setCellValueFactory(new PropertyValueFactory<>("laskutusTyyppi"));
+        
+        try {
+            // Luodaan Connection String olemassa olevaan tietokantaan
+            Connection conn = vuokratoimistoDatabase.openConnection("jdbc:mariadb://maria.westeurope.cloudapp.azure.com:"
+                    + "3306?user=opiskelija&password=opiskelija1");
+            
+            // Otetaan tietokanta kayttoon
+            vuokratoimistoDatabase.useDatabase(conn, "karelia_vuokratoimistot_R01");
+            
+            // Haetaan tiedot tietokannasta
+            ResultSet namesResult = selectLasku(conn);
+            
+            // Lisätään uudet person luokan ilmentymät TableView komponenttiin
+            while (namesResult.next()) {
+                Lasku person = new Lasku(namesResult.getInt("laskuID"), namesResult.getInt("asiakasID"), namesResult.getDate("erapaiva"),
+                        namesResult.getDate("maksupaiva"), namesResult.getInt("summa"), namesResult.getInt("maksettu"), namesResult.getString("laskutusTyyppi"));
+                tableLasku.getItems().add(person);
+            }
+            
+            //Suljetaan yhteys
+            vuokratoimistoDatabase.closeConnection(conn);
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(AsiakkaidenHallintaViewController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
     
     //valitsee asiakkaiden tiedot tableview:n täyttämistä varten
      public static ResultSet selectLasku(Connection c) throws SQLException {
