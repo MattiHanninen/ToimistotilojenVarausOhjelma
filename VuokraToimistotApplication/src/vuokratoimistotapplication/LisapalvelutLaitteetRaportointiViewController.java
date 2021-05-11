39af94dbcc25c52d9233736720d69920e32d9747
Matti Hänninen <matti.hanninen@edu.karelia.fi>
2021-05-11 13:26:34 +0300

    Palveluiden raportointi html lisätty, mutta lisää vain yhden rivin

--- VuokraToimistotApplication/src/vuokratoimistotapplication/LisapalvelutLaitteetRaportointiViewController.java
+++ VuokraToimistotApplication/src/vuokratoimistotapplication/LisapalvelutLaitteetRaportointiViewController.java
@@ -5,13 +5,19 @@
  */
 package vuokratoimistotapplication;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
+import static java.nio.charset.StandardCharsets.UTF_8;
+import java.nio.file.Files;
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import javafx.event.ActionEvent;
@@ -23,6 +29,8 @@ import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 import vuokratoimistotDatabase.vuokratoimistoDatabase;
 import vuokratoimistotapplication.Luokat.Palvelu;
@@ -67,6 +75,14 @@ public class LisapalvelutLaitteetRaportointiViewController implements Initializa
     
     java.sql.Date sqlAloitusPaiva;
     java.sql.Date sqlLopetusPaiva;
+    
+    private static final String TOIMIPISTEID = "__TOIMIPISTEID__";
+    private static final String TOIMIPISTENIMI = "__TOIMIPISTENIMI__";
+    private static final String ALOITUSPAIVA = "__ALOITUSPAIVA__";
+    private static final String LOPETUSPAIVA = "__LOPETUSPAIVA__";
+    private static final String TABLE_CONTENT = "__TABLE__";
+    @FXML
+    private WebView webView;
     
 
     /**
@@ -253,5 +269,117 @@ public class LisapalvelutLaitteetRaportointiViewController implements Initializa
         
         return rs;  
     }
+
+    @FXML
+    private void menuItemSaveClicked(ActionEvent event) throws IOException {
+        
+        
+        try {
+            
+
+            
+            // Aukaistaan tietokantayhteys
+            Connection conn = vuokratoimistoDatabase.openConnection("jdbc:mariadb://maria.westeurope.cloudapp.azure.com:"
+                    + "3306?user=opiskelija&password=opiskelija1");
+
+            // Asetetaan oikea tietokanta yhteydessa valituksi
+            vuokratoimistoDatabase.useDatabase(conn, "karelia_vuokratoimistot_R01");
+
+            // Haetaan tiedot tekstikentista               
+            sqlAloitusPaiva = java.sql.Date.valueOf(txtAloitusPaivaDate.getValue());
+            sqlLopetusPaiva = java.sql.Date.valueOf(txtLopetusPaivaDate.getValue());
+                    
+            // Haetaan tiedot kolmesta eri taulusta
+            ResultSet allInfoFromThreeTables = selectAllInfoFromThreeTables(conn, cboValitseToimipiste.getValue(), sqlAloitusPaiva, sqlLopetusPaiva);
+            
+            
+
+               while (allInfoFromThreeTables.next()) {
+                   
+                    // 1. kopioi pohja
+                    File source = new File("./Report/VuokratutPalvelutRaporttiTemplate.html");
+                    File dest = new File("./Report/VuokratutPalvelutTallennaRaportti.html");
+
+                    Files.copy(source.toPath(), dest.toPath(), REPLACE_EXISTING);
+
+                   
+                   
+                    // 2. Korvaa pohjassa olevat paikat tiedoilla
+                    String content = new String(Files.readAllBytes(dest.toPath()), UTF_8);
+                    //content = content.replaceAll(TOIMIPISTEID, Integer.toString(allInfoFromThreeTables.getInt("toimipisteID")));
+                    content = content.replaceAll(TOIMIPISTEID, cboValitseToimipiste.getValue().toString());
+                  
+                    Statement stmt = conn.createStatement();
+                    ResultSet toimipisteJaIDInfoResult = stmt.executeQuery(
+                    "SELECT toimipisteNimi FROM toimipiste WHERE toimipisteID LIKE '%" + cboValitseToimipiste.getValue()+"%'");
+                    
+                    while (toimipisteJaIDInfoResult.next()) {
+                        content = content.replaceAll(TOIMIPISTENIMI, toimipisteJaIDInfoResult.getString("toimipisteNimi"));
+                    }
+                    
+                    
+                    //content = content.replaceAll(ALOITUSPAIVA, allInfoFromThreeTables.getDate("aloitusPaiva").toString());
+                    content = content.replaceAll(ALOITUSPAIVA, txtAloitusPaivaDate.getValue().toString());
+                    content = content.replaceAll(LOPETUSPAIVA, txtLopetusPaivaDate.getValue().toString());
+                    
+                    
+                    content = content.replaceAll(TABLE_CONTENT, createRow(allInfoFromThreeTables.getInt("palvelunID"),
+                            allInfoFromThreeTables.getString("palvelunNimi"), allInfoFromThreeTables.getInt("palvelunHinta"), allInfoFromThreeTables.getString("palvelunKuvaus"),
+                            allInfoFromThreeTables.getInt("varausID"), allInfoFromThreeTables.getDate("aloitusPaiva"), allInfoFromThreeTables.getDate("lopetusPaiva"),
+                            allInfoFromThreeTables.getInt("asiakasID"), allInfoFromThreeTables.getInt("toimipisteID")));
+                
+                 
+                    // 3. Lataa muodostettu lasku
+                    Files.write(dest.toPath(), content.getBytes(UTF_8));
+        
+                    // 4. Näytä muodostettu lasku
+                    loadWebPage(dest.toPath().toString());
+                    
+                }
+               
+
+                   
+            // Suljetaan tietokantayhteys
+            vuokratoimistoDatabase.closeConnection(conn);
+        }
+        
+        // Napataan kiinni mahdolliset SQL poikkeukset
+        catch (SQLException ex) {
+            System.out.println("Catchiin meni");       
+            java.util.logging.Logger.getLogger(ToimipisteidenHallintaViewController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+
+    @FXML
+    private void menuItemPreviewClicked(ActionEvent event) {
+        
+        loadWebPage("./Report/VuokratutPalvelutRaporttiTemplate.html");
+    }
+    
+    private void loadWebPage(String path){
+        WebEngine engine = webView.getEngine();
+        File f = new File(path);
+        engine.load(f.toURI().toString());
+    }
+    
+    // Muodostetaan laskun yksi rivi html muodossa
+    private String createRow(int palvelunID, String palvelunNimi, int palvelunHinta, String palvelunKuvaus, int varausID, Date aloitusPaiva, Date lopetusPaiva, int asiakasID, int toimipisteID){
+        
+        return "<tr>" +
+                "<td class=\"palvelunID\">" + palvelunID + "</td>" + 
+                "<td class=\"palvelunNimi\">" + palvelunNimi + "</td>" +
+                "<td class=\"palvelunHinta\">" + palvelunHinta + "€</td>" +
+                "<td class=\"palvelunKuvaus\">" + palvelunKuvaus + "</td>" + 
+                "<td class=\"varausID\">" + varausID + "</td>" +
+                "<td class=\"aloitusPaiva\">" + aloitusPaiva + "</td>" +
+                "<td class=\"lopetusPaiva\">" + lopetusPaiva + "</td>" +
+                "<td class=\"asiakasID\">" + asiakasID + "</td>" +
+                "<td class=\"toimipisteID\">" + toimipisteID + "</td>"
+                ;
+        
+    }
+    
+    
     
 }
